415bf29c4f1b10ce04b74b5018c540784a9d1a40
unknown <artemaskel@gmail.com>
2024-12-13 11:05:39 +0300

    last

--- src/App.jsx
+++ src/App.jsx
@@ -3,128 +3,208 @@ import { useState, useEffect } from "react";
 
 import Header from "./components/header/header.jsx";
 import Background from "./components/background/background.jsx";
-import styleses from './components/cart/styles.module.css'
+import styleses from "./components/cart/styles.module.css";
 
 import menu from "./menu.js";
 import openCart from "./assets/cart.png";
-import cross from './assets/cross.png'
-import back from './assets/back.png'
-
+import cross from "./assets/cross.png";
+import back from "./assets/back.png";
 
 function App() {
   const [cart, setCart] = useState(false);
   const [items, setItems] = useState([]);
+  const [discount, setDiscount] = useState(0);
+  const tg = window.Telegram.WebApp
 
-  useEffect(() =>{
-    for(let i = 0; i < items.length; i++){
-      if(items[i].amount == 0){
-        setItems(items.filter(item => item.amount != 0))
+  useEffect(() => {
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].amount == 0) {
+        setItems(items.filter((item) => item.amount != 0));
       }
     }
-    mapping(items)
-  }, [items])
+    mapping(items);
 
-  function mapping(arg){
-		if(arg.length == 0){
-			return(
-				<>
-					<div className={styleses.emptyCart}>
-						Корзина пуста
-					</div>
-				</>
-			)
-		}	
-		else{
-			return arg.map(
-				({id, photo, name, price, amount}) => (
-				<div key={name} name={id} id={id} className={styleses.item}>
-					<div className={styleses.photoContainer}>
-						<img src={photo} alt="Товар" className={styleses.photo}/>
-					</div>
-					<div className={styleses.info}>
-						<div className={styleses.price}>{price*amount} руб.</div>
-						<div className={styleses.name}>{name}</div>
-						<div className={styleses.amount}>Количество: {amount}</div>
-					</div>
-					<div className={styleses.delete} onClick={(info) => {
-						setItems(items.filter(el => el.id !== info.target.name))
-						console.log(items);
-					}}>
-						<img name={id} src={cross} alt="Удалить" className={styleses.cross} />
-					</div>
-				</div>
-			)
-		)}
-	}
+    const sum = items.reduce((sum, item) => sum + item.price * item.amount, 0);
+    if (sum == 0) {
+      setDiscount(0);
+    }
+    if (sum > 0 && sum < 20) {
+      setDiscount(5);
+    }
+    if (sum >= 50) {
+      setDiscount(7);
+    }
+  }, [items]);
 
+  function mapping(arg) {
+    if (arg.length == 0) {
+      return (
+        <>
+          <div className={styleses.emptyCart}>Корзина пуста</div>
+        </>
+      );
+    } else {
+      return arg.map(({ id, photo, name, price, amount }) => (
+        <div key={name} name={id} id={id} className={styleses.item}>
+          <div className={styleses.photoContainer}>
+            <img src={photo} alt="Товар" className={styleses.photo} />
+          </div>
+          <div className={styleses.info}>
+            <div className={styleses.name}>{name}</div>
+            <div className={styleses.price}>{price * amount} руб.</div>
+            <div className={styleses.amount}>Количество: {amount}</div>
+          </div>
+          <div
+            className={styleses.delete}
+            onClick={(info) => {
+              setItems(items.filter((el) => el.id !== info.target.name));
+            }} 
+          >
+            <img
+              name={id}
+              src={cross}
+              alt="Удалить"
+              className={styleses.cross}
+            />
+          </div>
+        </div>
+      ));
+    }
+  }
 
-  function add({click, id, name, price, photo}){
-    click.target.style.width = "46%"
-    if (items.some(item => item.id === id)  ) {
-      const updatedItems = items.map(item =>
-        item.id === id? {...item, amount: item.amount + 1 } : item
+  function add({ click, id, name, price, photo }) {
+    click.target.style.width = "46%";
+    if (items.some((item) => item.id === id)) {
+      const updatedItems = items.map((item) =>
+        item.id === id ? { ...item, amount: item.amount + 1 } : item
       );
       setItems(updatedItems);
     } else {
-      const newItems = [...items, { id: id, name: name, price: price, photo: photo, amount: 1 }];
-      setItems(newItems)
+      const newItems = [
+        ...items,
+        { id: id, name: name, price: price, photo: photo, amount: 1 },
+      ];
+      setItems(newItems);
     }
   }
 
-  function remove({click, id}){
-    if (items.some(item => item.id === id)  ) {
-      const item = items.filter(item => item.id === id)[0]
-
-      console.log(item);
-      if(item.amount >= 1){
-        const updatedItems = items.map(item =>
-          item.id === id? {...item, amount: item.amount - 1 } : item
+  function remove({ id }) {
+    if (items.some((item) => item.id === id)) {
+      const item = items.filter((item) => item.id === id)[0];
+      if (item.amount >= 1) {
+        const updatedItems = items.map((item) =>
+          item.id === id ? { ...item, amount: item.amount - 1 } : item
         );
         setItems(updatedItems);
-      } else {
-        console.log('none');
       }
-
-
-
-    } else {
-      console.log("there is no such items");
+      if (item.amount == 1) {
+        const add_b = document.getElementsByName(id);
+        add_b[0].style.width = "130px";
+      }
     }
-
   }
 
 
 
 
+  function buy(){
+    if(items.length != 0){
+      const total = items.reduce((sum, item) => sum + item.price * item.amount, 0) -
+      (
+        (items.reduce(
+          (sum, item) => sum + item.price * item.amount,
+          0
+        ) *
+          discount) /
+        100
+      ).toFixed(2)
 
-//////////////////////////////////////////////////////////////////////////////////////////////////
+      const sum = items
+      .reduce((sum, item) => sum + item.price * item.amount, 0)
+      .toFixed(2)
+      tg.sendData([items, [sum, (sum*discount/100).toFixed(2), total]])
+      setItems([]);
+    }
+  }
+
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
   return (
     <div className={styles.main}>
       {cart ? (
         <>
-            <Background />
-            <div className={styleses.cart}>
-              <div className={styleses.head} onClick={() => {console.log(items)}}>
-                Корзина
-              </div>
-              <div className={styleses.items}>
-                {mapping(items)}
-              </div>
+          <Background />
+          <div className={styleses.cart}>
+            <div
+              className={styleses.head}>
+              Корзина
             </div>
+            <div className={styleses.items}>{mapping(items)}</div>
+          </div>
           <div className={styles.closeCart} onClick={() => setCart(!cart)}>
             <img src={back} alt="Закрыть корзину" />
           </div>
+          <div className={styles.sum}>
+            Товары({items.reduce((all, item) => all + item.amount, 0)}):
+            <div>
+              {items
+                .reduce((sum, item) => sum + item.price * item.amount, 0)
+                .toFixed(2)}{" "}
+              руб.
+            </div>
+          </div>
+
+          <div className={styles.discount}>
+            Скидка:{" "}
+            <div>
+              {" "}
+              -
+              {(
+                (items.reduce(
+                  (sum, item) => sum + item.price * item.amount,
+                  0
+                ) *
+                  discount) /
+                100
+              ).toFixed(2)}{" "}
+              руб.{" "}
+            </div>
+          </div>
+
+          <div className={styles.total}>
+            Итого:
+            <div>
+              {(
+                items.reduce((sum, item) => sum + item.price * item.amount, 0) -
+                (
+                  (items.reduce(
+                    (sum, item) => sum + item.price * item.amount,
+                    0
+                  ) *
+                    discount) /
+                  100
+                ).toFixed(2)
+              ).toFixed(2)}{" "}
+              руб.
+            </div>
+          </div>
+          <button
+            className={styles.buy}
+            id='order'
+            onClick={() => buy()}
+          >
+            Оформить заказ
+          </button>
         </>
       ) : (
         <>
           <Background />
           <Header />
           <div className={styles.menu}>
-            <p className={styles.black} onClick={
-              () => {
-                console.log(items);
-              }
-            } id="black">
+            <p
+              className={styles.black}
+              id="black"
+            >
               Black
             </p>
             {menu.black.map(({ id, name, g, price, photo }) => {
@@ -139,11 +219,22 @@ function App() {
                     <div className={styles.price}>{price} руб.</div>
                   </div>
                   <div className={styles.addToCart}>
-                    <div className={styles.remove} onClick={(click) => remove({click, id})}></div>
-                    <div className={styles.add} onClick={(click) => add({click, id, name, price, photo})}></div>
+                    <div
+                      className={styles.remove}
+                      onClick={(click) => remove({ click, id })}
+                    ></div>
+                    <div
+                      className={styles.add}
+                      name={id}
+                      onClick={(click) =>
+                        add({ click, id, name, price, photo })
+                      }
+                    ></div>
                   </div>
                   <div className={styles.amount}>
-                      {items.some(item => item.id === id)? items.filter(item => item.id === id)[0].amount : 0}
+                    {items.some((item) => item.id === id)
+                      ? items.filter((item) => item.id === id)[0].amount
+                      : 0}
                   </div>
                 </div>
               );
